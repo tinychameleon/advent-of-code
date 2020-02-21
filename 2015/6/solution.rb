b49a07f4569076e01cf0f90447e649a8532bf214
Stephan Tarulli <srt@tinychameleon.com>
2020-02-20 18:42:05 -0800

    2015-6: Solution to part B

--- 2015/6/solution.rb
+++ 2015/6/solution.rb
@@ -6,6 +6,12 @@ Point = Struct.new(:x, :y) do
   end
 end
 
+class Integer
+  def max(i)
+    self > i ? self : i
+  end
+end
+
 class LightGrid
   def initialize(val, rows: 1_000, cols: 1_000)
     @grid = [val] * rows * cols
@@ -58,11 +64,36 @@ class SwitchLightGrid < LightGrid
   end
 end
 
+class DimmableLightGrid < LightGrid
+  def initialize(rows: 1_000, cols: 1_000)
+    super(0, rows: rows, cols: cols)
+  end
+
+  def brightness
+    @grid.sum
+  end
+
+  def turn_on(origin, bound)
+    change_state(origin, bound) { |i| i + 1 }
+  end
+
+  def turn_off(origin, bound)
+    change_state(origin, bound) { |i| 0.max(i - 1) }
+  end
+
+  def toggle(origin, bound)
+    change_state(origin, bound) { |i| i + 2 }
+  end
+end
+
 class Solution
   def tests
     test_switchlightgrid
     test_parse_line
     test_solve_a
+
+    test_dimmablelightgrid
+    test_solve_b
     :ok
   end
 
@@ -71,7 +102,7 @@ class Solution
   end
 
   def part_b
-    raise NotImplementedError
+    solve_b(DimmableLightGrid.new, File.read('input'))
   end
 
   private
@@ -102,6 +133,32 @@ class Solution
     assert g.count, 3
   end
 
+  def test_dimmablelightgrid
+    g = DimmableLightGrid.new(rows: 2, cols: 2)
+    #   0 1
+    # 0 0 0
+    # 1 0 0
+    assert g.brightness, 0
+
+    #   0 1
+    # 0 1 0
+    # 1 1 0
+    g.turn_on(Point[0, 0], Point[0, 1])
+    assert g.brightness, 2
+
+    #   0 1
+    # 0 1 0
+    # 1 0 0
+    g.turn_off(Point[0, 1], Point[1, 1])
+    assert g.brightness, 1
+
+    #   0 1
+    # 0 3 2
+    # 1 2 2
+    g.toggle(Point[0, 0], Point[1, 1])
+    assert g.brightness, 9
+  end
+
   def test_parse_line
     assert parse_line('turn on 0,0 through 999,999'), [
       :on, Point[0, 0], Point[999, 999]
@@ -124,6 +181,16 @@ class Solution
     assert solve_a(g, input), 3
   end
 
+  def test_solve_b
+    g = DimmableLightGrid.new(rows: 2, cols: 2)
+    input = <<~data
+      turn on 0,0 through 0,1
+      turn off 0,1 through 1,1
+      toggle 0,0 through 1,1
+    data
+    assert solve_b(g, input), 9
+  end
+
   MATCHERS = {
     on: /turn on (\d+),(\d+) through (\d+),(\d+)/,
     off: /turn off (\d+),(\d+) through (\d+),(\d+)/,
@@ -144,7 +211,7 @@ class Solution
     [Point[ps[0], ps[1]], Point[ps[2], ps[3]]]
   end
 
-  def solve_a(grid, input)
+  def solve_worker(grid, input)
     input.split("\n").map { |l| parse_line(l) }.each do |action, origin, bound|
       case action
       when :on
@@ -155,10 +222,15 @@ class Solution
         grid.toggle(origin, bound)
       end
     end
+  end
+
+  def solve_a(grid, input)
+    solve_worker(grid, input)
     grid.count
   end
 
-  def solve_b(input)
-    raise NotImplementedError
+  def solve_b(grid, input)
+    solve_worker(grid, input)
+    grid.brightness
   end
 end
