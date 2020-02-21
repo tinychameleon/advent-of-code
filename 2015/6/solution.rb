f9bb19f28698c63a40c17693bba11545cbd9171f
Stephan Tarulli <srt@tinychameleon.com>
2020-02-20 18:15:50 -0800

    2015-6: Make Point construction better

--- 2015/6/solution.rb
+++ 2015/6/solution.rb
@@ -1,6 +1,10 @@
 require './utils'
 
-Point = Struct.new(:x, :y)
+Point = Struct.new(:x, :y) do
+  def self.[](x, y)
+    Point.new(x, y)
+  end
+end
 
 class LightGrid
   def initialize(rows: 1_000, cols: 1_000)
@@ -64,31 +68,31 @@ class Solution
     #   0 1
     # 0 + -
     # 1 + -
-    g.turn_on(Point.new(0, 0), Point.new(0, 1))
+    g.turn_on(Point[0, 0], Point[0, 1])
     assert g.count, 2
 
     #   0 1
     # 0 + -
     # 1 - -
-    g.turn_off(Point.new(0, 1), Point.new(1, 1))
+    g.turn_off(Point[0, 1], Point[1, 1])
     assert g.count, 1
 
     #   0 1
     # 0 - +
     # 1 + +
-    g.toggle(Point.new(0, 0), Point.new(1, 1))
+    g.toggle(Point[0, 0], Point[1, 1])
     assert g.count, 3
   end
 
   def test_parse_line
     assert parse_line('turn on 0,0 through 999,999'), [
-      :on, Point.new(0, 0), Point.new(999, 999)
+      :on, Point[0, 0], Point[999, 999]
     ]
     assert parse_line('toggle 0,0 through 999,0'), [
-      :toggle, Point.new(0, 0), Point.new(999, 0)
+      :toggle, Point[0, 0], Point[999, 0]
     ]
     assert parse_line('turn off 499,499 through 500,500'), [
-      :off, Point.new(499, 499), Point.new(500, 500)
+      :off, Point[499, 499], Point[500, 500]
     ]
   end
 
@@ -119,7 +123,7 @@ class Solution
 
   def make_points(captures)
     ps = captures.map(&:to_i)
-    [Point.new(ps[0], ps[1]), Point.new(ps[2], ps[3])]
+    [Point[ps[0], ps[1]], Point[ps[2], ps[3]]]
   end
 
   def solve_a(grid, input)
