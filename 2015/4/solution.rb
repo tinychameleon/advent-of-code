0fc0e3b48899df2f5c7feba7c026d7cd04040a37
Stephan Tarulli <srt@tinychameleon.com>
2020-02-20 17:58:25 -0800

    2015-4: Solution for part B

--- 2015/4/solution.rb
+++ 2015/4/solution.rb
@@ -3,24 +3,25 @@ require 'digest'
 
 class Solution
   def tests
-    assert solve('abcdef'), 609_043
+    assert solve('abcdef', zeroes: 5), 609_043
+    assert solve('pqrstuv', zeroes: 5), 1_048_970
     :ok
   end
 
   INPUT = 'bgvyzdsv'.freeze
 
   def part_a
-    solve(INPUT)
+    solve(INPUT, zeroes: 5)
   end
 
   def part_b
-    raise NotImplementedError
+    solve(INPUT, zeroes: 6)
   end
 
   private
 
-  def solve(input)
-    prefix = '0' * 5
+  def solve(input, zeroes:)
+    prefix = '0' * zeroes
     (1..).each do |i|
       hex = Digest::MD5.hexdigest "#{input}#{i}"
       return i if hex.start_with? prefix
