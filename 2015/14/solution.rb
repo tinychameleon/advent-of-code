a69ed0c7aaebc47a763546ec5dfb1073eb42a0d2
Stephan Tarulli <srt@tinychameleon.com>
2020-02-29 18:55:41 -0800

    2015-14: Solution for part B

--- 2015/14/solution.rb
+++ 2015/14/solution.rb
@@ -7,6 +7,8 @@ class Solution
 
   TEST_DATA = { velocity: 14, duration: 10, resting: 127 }.freeze
 
+  SECONDS = 2503
+
   def tests
     assert parse_line(TEST_INPUT), TEST_DATA
     assert distance(TEST_DATA, 1), 14
@@ -14,15 +16,19 @@ class Solution
     assert distance(TEST_DATA, 11), 140
     assert distance(TEST_DATA, 138), 154
     assert distance(TEST_DATA, 1000), 1120
+
+    assert simulate(TEST_DATA, 10).last, 140
+    assert simulate(TEST_DATA, 138).last, 154
+    assert simulate(TEST_DATA, 1000).last, 1120
     :ok
   end
 
   def part_a
-    solve_a(File.read('input'))
+    solve_a(File.read('input'), SECONDS)
   end
 
   def part_b
-    raise NotImplementedError
+    solve_b(File.read('input'), SECONDS)
   end
 
   private
@@ -39,11 +45,20 @@ class Solution
     reindeer[:velocity] * (cycles * reindeer[:duration] + remainder)
   end
 
-  def solve_a(input)
-    input.lines.map { |l| parse_line(l) }.map { |r| distance(r, 2503) }.max
+  def simulate(reindeer, seconds)
+    (1..seconds).map { |s| distance(reindeer, s) }
   end
 
-  def solve_b(_input)
-    raise NotImplementedError
+  def solve_a(input, seconds)
+    input.lines.map { |l| parse_line(l) }.map { |r| distance(r, seconds) }.max
+  end
+
+  def solve_b(input, seconds)
+    reindeer = input.lines.map { |l| simulate(parse_line(l), seconds) }
+    (0...seconds).each do |i|
+      leader = reindeer.map { |km| km[i] }.max
+      reindeer.each { |km| km[i] = km[i] == leader ? 1 : 0 }
+    end
+    reindeer.map(&:sum).max
   end
 end
