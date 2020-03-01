0c93992826fb16bd257dd29bf892e99aca6ead08
Stephan Tarulli <srt@tinychameleon.com>
2020-03-01 15:38:20 -0800

    2015-17: Solution to part B

--- 2015/17/solution.rb
+++ 2015/17/solution.rb
@@ -9,9 +9,13 @@ class Solution
     5
   DATA
 
+  TEST_DATA = [20, 15, 10, 5, 5].freeze
+
   def tests
-    assert parse_containers(TEST_INPUT), [20, 15, 10, 5, 5]
-    assert storage_ways([20, 15, 10, 5, 5], 25), 4
+    assert parse_containers(TEST_INPUT), TEST_DATA
+    assert minimum_containers(TEST_DATA, 25), 2
+    assert storage_ways(TEST_DATA, 2, 25), 3
+    assert all_storage_ways(TEST_DATA, 25), 4
     :ok
   end
 
@@ -20,7 +24,7 @@ class Solution
   end
 
   def part_b
-    raise NotImplementedError
+    solve_b(File.read('input'))
   end
 
   private
@@ -29,17 +33,26 @@ class Solution
     input.lines.map(&:to_i)
   end
 
-  def storage_ways(containers, amount)
-    (1..containers.size).map do |s|
-      containers.combination(s).count { |c| c.sum == amount }
-    end.sum
+  def storage_ways(containers, choose, amount)
+    containers.combination(choose).count { |c| c.sum == amount }
+  end
+
+  def minimum_containers(containers, amount)
+    (1..containers.size).each do |s|
+      return s if containers.combination(s).any? { |c| c.sum == amount }
+    end
+  end
+
+  def all_storage_ways(containers, amount)
+    (1..containers.size).map { |s| storage_ways(containers, s, amount) }.sum
   end
 
   def solve_a(input)
-    storage_ways(parse_containers(input), 150)
+    all_storage_ways(parse_containers(input), 150)
   end
 
-  def solve_b(_input)
-    raise NotImplementedError
+  def solve_b(input)
+    containers = parse_containers(input)
+    storage_ways(containers, minimum_containers(containers, 150), 150)
   end
 end
