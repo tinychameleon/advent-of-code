d7443f09e27d2110f161e799cfbf3f3ab2fb5a54
Stephan Tarulli <srt@tinychameleon.com>
2020-03-03 19:04:40 -0800

    2015-20: Solution to part A

---
+++ 2015/20/solution.rb
@@ -0,0 +1,46 @@
+require './utils'
+
+class Solution
+  def tests
+    assert factors(6).sort, [1, 2, 3, 6]
+    assert factors(7).sort, [1, 7]
+    assert factors(1).sort, [1]
+    assert factors(9).sort, [1, 3, 9]
+
+    :ok
+  end
+
+  INPUT = 29_000_000
+
+  def part_a
+    solve_a(INPUT)
+  end
+
+  def part_b
+    raise NotImplementedError
+  end
+
+  private
+
+  def factors(n)
+    fs = [1, n]
+    (2..Math.sqrt(n)).each do |i|
+      next if n % i != 0
+
+      fs << i
+      fs << n / i
+    end
+    fs.uniq
+  end
+
+  def solve_a(input)
+    target = input / 10
+    (1..).each do |house|
+      return house if target < factors(house).sum
+    end
+  end
+
+  def solve_b(input)
+    raise NotImplementedError
+  end
+end
